a36fef85c1c09b5b2c96274b364015f7a2dfd2a0
Yunzhuo Jiang <jiang3168@gmail.com>
2025-11-04 21:21:29 +0100

    modify index.py

--- api/index.py
+++ api/index.py
@@ -1,17 +1,29 @@
 import os
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import StreamingResponse
 from google import genai
 
 app = FastAPI()
 
-@app.get("/api", response_class=PlainTextResponse)
+@app.get("/api", response_class=StreamingResponse)
 def idea():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    prompt = "Come up with a new business idea for AI agents"
+    prompt = "Come up with one new business idea for AI agents, keep it short and concise."
     response = client.models.generate_content(
         model = "gemini-2.5-flash",
         contents = prompt,
+        stream = True,
     )
-    return response.text
+
+    def event_stream():
+        for chunk in response:
+            text = chunk.text
+            if text:
+                lines = text.split("\n")
+                for line in lines:
+                    yield f"data: {line}\n"
+                yield "\n"
+
+
+    return StreamingResponse(event_stream(), media_type="text/event_stream")
